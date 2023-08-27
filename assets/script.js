b00d515252fc8b825cbfe9b9428c669fb56380a1
McleodHerritt <nmcleod2021@gmail.com>
2023-08-27 15:21:38 -0300

    wrote a function to tell if the given hour is in the past, present or future. Updated the hour blocks with classes past, prestent or future.

--- assets/script.js
+++ assets/script.js
@@ -9,19 +9,39 @@ $(document).ready(function () {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
+  //
+  // Apply the past, present, or future class to each time
+  // block by comparing the id to the current hour.
+  for (var hour = 9; hour <= 17; hour++) {
+    if (checkHour(hour) == -1) {
+      $("#hour-" + hour).addClass("past");
+    } else if (checkHour(hour) == 0) {
+      $("#hour-" + hour).addClass("present");
+    } else {
+      $("#hour-" + hour).addClass("future");
+    }
+  }
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
 
   // Extend dayjs with the advancedFormat plugin
   dayjs.extend(window.dayjs_plugin_advancedFormat);
   $("#currentDay").text(dayjs().format("dddd, MMMM Do"));
 });
+
+// Ruturns -1 if in the past, returns 0 in the present, returns +1 in the future.
+// Hour will be in 24 hr clock.
+function checkHour(hour) {
+  var currentHour = dayjs().hour();
+
+  if (currentHour > hour) {
+    return -1;
+  } else if (currentHour == hour) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
